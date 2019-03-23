c1fae24fc9b1c5a2da03aaa2edb22519f97eefe6
darylm20 <daryl.mawere@my.jcu.edu.au>
2019-03-23 23:10:55 +1000

    First

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -6,12 +6,12 @@ MAXIMUM = 45
 
 
 def main():
-    number_of_quick_picks = int(input("How many quick picks would you like? "))
-    while number_of_quick_picks < 0:
+    no_quick_picks = int(input("How many quick picks would you like? "))
+    while no_quick_picks < 0:
         print("Does not compute! Please pick a number greater than 0 (zero)")
-        number_of_quick_picks = int(input("How many quick picks would you like? "))
+        no_quick_picks = int(input("How many quick picks would you like? "))
 
-    for a in range(number_of_quick_picks):
+    for a in range(no_quick_picks):
         quick_pick = []
         for b in range(NUMBERS_PER_LINE):
             number = random.randint(MINIMUM, MAXIMUM)
